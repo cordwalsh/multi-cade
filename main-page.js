810e8ccb113b3e6509bf5a7573d50736f2ac8b43
Student <example@example.com>
2019-07-11 11:22:52 -0700

    add links to gh-pages

--- main-page.js
+++ main-page.js
@@ -21,11 +21,6 @@ $(document).ready(function () {
     $(this).removeClass('z-depth-5 grow still');
   });
 
-  $('#card1').mouseenter(function () {
-    $('#gif1').show();
-    $('.still').hide();
-  });
-
   $('#card1').click(function () {
     $('#main-page').hide();
     $('#asteroids').fadeIn();
@@ -33,18 +28,58 @@ $(document).ready(function () {
     window.scrollTo(0, 0);
   });
 
+  $('#card2').click(function(){
+    window.location.href = "https://pdxjess.github.io/froggy";
+  })
+
+  $('#card3').click(function(){
+    window.location.href = "https://cordwalsh.github.io/kirby-flight";
+  })
+
+  $('#card4').click(function(){
+    window.location.href = "https://ajrmdn.github.io/snake";
+  })
+
+
+  $('#card1').mouseenter(function () {
+    $('#gif1').show();
+    $('.still').hide();
+  });
+
+
   $('#card1').mouseleave(function () {
     $('#gif1').hide();
     $('.still').show();
   });
 
-  $('#card2').mouseenter(function () {
+  $('#card3').mouseenter(function () {
     $('#gif2').show();
     $('.still2').hide();
   });
 
-  $('#card2').mouseleave(function () {
+  $('#card3').mouseleave(function () {
     $('#gif2').hide();
     $('.still2').show();
+  });
+
+  $('#card4').mouseenter(function () {
+    $('#gif4').show();
+    $('.still4').hide();
+  });
+
+
+  $('#card4').mouseleave(function () {
+    $('#gif4').hide();
+    $('.still4').show();
+  });
+
+  $('#card2').mouseenter(function () {
+    $('#gif3').show();
+    $('.still3').hide();
+  });
+
+  $('#card2').mouseleave(function () {
+    $('#gif3').hide();
+    $('.still3').show();
   });
 });
